7ab2be1b4f7933857b61c7e831c197f392ebbe3a
Arthur Machado <arthur.machado@iclubs.tech>
2023-08-26 23:41:46 -0300

    add session_id migration

--- db/migrations/20230827005150_register-meals.ts
+++ db/migrations/20230827005150_register-meals.ts
@@ -3,7 +3,7 @@ import { Knex } from 'knex'
 export async function up(knex: Knex): Promise<void> {
   await knex.schema.createTable('meals', (table) => {
     table.uuid('id').primary()
-    table.uuid('user_id').references('users.id')
+    table.uuid('user_id').references('users.id').index()
     table.text('name').notNullable()
     table.text('description')
     table.text('meal_date').notNullable()
