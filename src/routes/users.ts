378427adf3829fe41e5028b3ca9ddc352d708a2f
Arthur Machado <arthur.machado@iclubs.tech>
2023-08-27 00:12:41 -0300

    create post user

---
+++ src/routes/users.ts
@@ -0,0 +1,30 @@
+import { FastifyInstance } from 'fastify'
+import { randomUUID } from 'node:crypto'
+
+import { createUserBodySchema } from '../schema/create-user-schema'
+import { knex } from '../database'
+
+export async function usersRoutes(app: FastifyInstance) {
+  app.post('/', async (request, reply) => {
+    const { email, password } = createUserBodySchema.parse(request.body)
+
+    let sessionId = request.cookies.sessionId
+
+    if (!sessionId) {
+      sessionId = randomUUID()
+
+      reply.cookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 1 * 60 * 60 * 24 * 7, // 7 days
+      })
+
+      await knex('users').insert({
+        id: randomUUID(),
+        email,
+        password,
+      })
+
+      return reply.status(201).send()
+    }
+  })
+}
